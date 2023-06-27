ff0dcd2925173e65e6d38ce6759ab2d5e46b6fe3
Akshay Padte <akshaypadte99@gmail.com>
2023-06-27 16:36:28 -0700

    feat: revert lab branding

--- cvat-ui/src/components/signing-common/signing-layout.tsx
+++ cvat-ui/src/components/signing-common/signing-layout.tsx
@@ -81,7 +81,7 @@ function SignInLayout(props: SignInLayoutComponentProps): JSX.Element {
                 <Content>
                     <Row justify='center' align='middle' style={{ height: '100%' }}>
                         <Col {...titleSizes} className='cvat-signing-title'>
-                            <Title>MOLA Lab</Title>
+                            <Title>Computer Vision</Title>
                             <Title>Annotation Platform</Title>
                         </Col>
                         {children}
